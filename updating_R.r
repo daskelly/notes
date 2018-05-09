130c24bc3d2f2626438a32af561dd8fc7cd38042
Dan Skelly <daskelly@users.noreply.github.com>
2018-05-09 14:38:23 -0400

    add caveat about devtools::install_github()

--- updating_R.r
+++ updating_R.r
@@ -9,3 +9,6 @@ save(packages, file="updating_R.RData")
 # After updating:
 load("updating_R.RData")
 install.packages(packages)
+
+# Will still need to reinstall all packages installed
+# using devtools::install_github()
