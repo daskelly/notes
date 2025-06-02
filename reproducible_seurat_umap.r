5c0cbc0837549a8f9570b04682c19ad663161661
Dan Skelly <daskelly@users.noreply.github.com>
2025-06-02 18:37:01 -0400

    add umap lib

--- reproducible_seurat_umap.r
+++ reproducible_seurat_umap.r
@@ -4,3 +4,4 @@ myenv <- new.env()
 source(paste0('https://raw.githubusercontent.com/daskelly/seurat/refs/heads/',
               'umap-approx-pow/R/dimensional_reduction.R'), local = myenv)
 RunUMAP.Seurat <- get("RunUMAP.Seurat", env = myenv)
+library(umap)
