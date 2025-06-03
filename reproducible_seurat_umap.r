b19e58f9c2af9bc58f12ef056f5f538f882ad2cf
Dan Skelly <daskelly@users.noreply.github.com>
2025-06-03 09:49:26 -0400

    correct error loading umap->uwot

--- reproducible_seurat_umap.r
+++ reproducible_seurat_umap.r
@@ -4,4 +4,5 @@ myenv <- new.env()
 source(paste0('https://raw.githubusercontent.com/daskelly/seurat/refs/heads/',
               'umap-approx-pow/R/dimensional_reduction.R'), local = myenv)
 RunUMAP.Seurat <- get("RunUMAP.Seurat", env = myenv)
-library(umap)
+library(uwot)
+
